2b5dd41d1510aa7c8fce1c4a6224e92324c38dd7
Dev Kumar <dev796699@gmail.com>
2024-03-11 13:17:38 +0530

    initial Commit

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -18,7 +18,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <div className='info-box'>
         <p className='font-medium sm:text-xl text-center'>
-          Worked with many companies <br /> and picked up many skills along the way
+          Worked at one company on many projects  <br /> and learned a lot of skills.
         </p>
 
         <Link to='/about' className='neo-brutalism-white neo-btn'>
